a6df458c6b3b996152c2e361255813a6f35137a5
William Mian <william_mian@yahoo.com.br>
2024-07-10 10:47:54 -0300

    modo assistant

--- src/main/java/br/com/wm/ecomart/chatbot/domain/service/ChatbotService.java
+++ src/main/java/br/com/wm/ecomart/chatbot/domain/service/ChatbotService.java
@@ -15,7 +15,7 @@ public class ChatbotService {
         this.client = client;
     }
 
-    public Flowable<ChatCompletionChunk> responderPergunta(String pergunta) {
+    public String responderPergunta(String pergunta) {
         var promptSistema = "Você é um chatbot de atendimento a clientes de um ecommerce e deve responder apenas perguntas relacionadas com o ecommerce";
         var dados = new DadosRequisicaoChatCompletion(promptSistema, pergunta);
         return client.enviarRequisicaoChatCompletion(dados);
--- src/main/java/br/com/wm/ecomart/chatbot/web/controller/ChatController.java
+++ src/main/java/br/com/wm/ecomart/chatbot/web/controller/ChatController.java
@@ -26,19 +26,8 @@ public class ChatController {
 
     @PostMapping
     @ResponseBody
-    public ResponseBodyEmitter responderPergunta(@RequestBody PerguntaDto dto) {
-        var fluxoResposta = service.responderPergunta(dto.pergunta());
-        var emitter = new ResponseBodyEmitter();
-
-        fluxoResposta.subscribe(chunk -> {
-           var token = chunk.getChoices().get(0).getMessage().getContent();
-           if(token != null) {
-               emitter.send(token);
-           }
-        }, emitter::completeWithError,
-                emitter::complete);
-
-        return emitter;
+    public String responderPergunta(@RequestBody PerguntaDto dto) {
+        return service.responderPergunta(dto.pergunta());
     }
 
     @GetMapping("limpar")
