9364c847dcbe88d48bbe4f2554b1d29495cacdd4
William Mian <william_mian@yahoo.com.br>
2024-07-11 11:13:42 -0300

    function calling

--- src/main/java/br/com/wm/ecomart/chatbot/infra/openai/OpenAIClient.java
+++ src/main/java/br/com/wm/ecomart/chatbot/infra/openai/OpenAIClient.java
@@ -1,13 +1,17 @@
 package br.com.wm.ecomart.chatbot.infra.openai;
 
+import br.com.wm.ecomart.chatbot.domain.DadosCalculoFrete;
+import br.com.wm.ecomart.chatbot.domain.service.CalculadorDeFrete;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.theokanning.openai.OpenAiHttpException;
-import com.theokanning.openai.completion.chat.ChatCompletionChunk;
-import com.theokanning.openai.completion.chat.ChatCompletionRequest;
-import com.theokanning.openai.completion.chat.ChatMessage;
-import com.theokanning.openai.completion.chat.ChatMessageRole;
+import com.theokanning.openai.completion.chat.*;
 import com.theokanning.openai.messages.Message;
 import com.theokanning.openai.messages.MessageRequest;
+import com.theokanning.openai.runs.Run;
 import com.theokanning.openai.runs.RunCreateRequest;
+import com.theokanning.openai.runs.SubmitToolOutputRequestItem;
+import com.theokanning.openai.runs.SubmitToolOutputsRequest;
+import com.theokanning.openai.service.FunctionExecutor;
 import com.theokanning.openai.service.OpenAiService;
 import com.theokanning.openai.threads.ThreadRequest;
 import io.reactivex.Flowable;
@@ -28,13 +32,107 @@ public class OpenAIClient {
     private final String assistantId;
     private String threadId;
     private final OpenAiService service;
+    private final CalculadorDeFrete calculadorDeFrete;
 
-    public OpenAIClient(@Value("${app.openai.api.key}") String apiKey, @Value("${app.openai.assistant.id}") String assistantId) {
+    public OpenAIClient(@Value("${app.openai.api.key}") String apiKey, @Value("${app.openai.assistant.id}") String assistantId, CalculadorDeFrete calculadorDeFrete) {
         this.apiKey = apiKey;
         this.service = new OpenAiService(apiKey, Duration.ofSeconds(60));
         this.assistantId = assistantId;
+        this.calculadorDeFrete = calculadorDeFrete;
     }
 
+    public String enviarRequisicaoChatCompletion(DadosRequisicaoChatCompletion dados) {
+        var messageRequest = MessageRequest
+                .builder()
+                .role(ChatMessageRole.USER.value())
+                .content(dados.promptUsuario())
+                .build();
+
+        if (this.threadId == null) {
+            var threadRequest = ThreadRequest
+                    .builder()
+                    .messages(Arrays.asList(messageRequest))
+                    .build();
+
+            var thread = service.createThread(threadRequest);
+            this.threadId = thread.getId();
+        } else {
+            service.createMessage(this.threadId, messageRequest);
+        }
+
+        var runRequest = RunCreateRequest
+                .builder()
+                .assistantId(assistantId)
+                .build();
+        var run = service.createRun(threadId, runRequest);
+
+        var concluido = false;
+        var precisaChamarFuncao = false;
+        try {
+            while (!concluido && !precisaChamarFuncao) {
+                Thread.sleep(1000 * 10);
+                run = service.retrieveRun(threadId, run.getId());
+                concluido = run.getStatus().equalsIgnoreCase("completed");
+                precisaChamarFuncao = run.getRequiredAction() != null;
+            }
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        if (precisaChamarFuncao) {
+            var precoDoFrete = chamarFuncao(run);
+            var submitRequest = SubmitToolOutputsRequest
+                    .builder()
+                    .toolOutputs(Arrays.asList(
+                            new SubmitToolOutputRequestItem(
+                                    run
+                                            .getRequiredAction()
+                                            .getSubmitToolOutputs()
+                                            .getToolCalls()
+                                            .get(0)
+                                            .getId(),
+                                    precoDoFrete)
+                    ))
+                    .build();
+            service.submitToolOutputs(threadId, run.getId(), submitRequest);
+
+            try {
+                while (!concluido) {
+                    Thread.sleep(1000 * 10);
+                    run = service.retrieveRun(threadId, run.getId());
+                    concluido = run.getStatus().equalsIgnoreCase("completed");
+                }
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        var mensagens = service.listMessages(threadId);
+        return mensagens
+                .getData()
+                .stream()
+                .sorted(Comparator.comparingInt(Message::getCreatedAt).reversed())
+                .findFirst().get().getContent().get(0).getText().getValue()
+                .replaceAll("\\\u3010.*?\\\u3011", "");
+    }
+
+    private String chamarFuncao(Run run) {
+        try {
+            var funcao = run.getRequiredAction().getSubmitToolOutputs().getToolCalls().get(0).getFunction();
+            var funcaoCalcularFrete = ChatFunction.builder()
+                    .name("calcularFrete")
+                    .executor(DadosCalculoFrete.class, d -> calculadorDeFrete.calcular(d))
+                    .build();
+
+            var executorDeFuncoes = new FunctionExecutor(Arrays.asList(funcaoCalcularFrete));
+            var functionCall = new ChatFunctionCall(funcao.getName(), new ObjectMapper().readTree(funcao.getArguments()));
+            return executorDeFuncoes.execute(functionCall).toString();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    /* knowledge retrieval ... não funcionando... A.4
     public String enviarRequisicaoChatCompletion(DadosRequisicaoChatCompletion dados) {
         var messageRequest = MessageRequest
                 .builder()
@@ -79,6 +177,7 @@ public class OpenAIClient {
 
         return respostaAssistente;
     }
+     */
 
     public List<String> carregarHistoricoDeMensagens() {
         var mensagens = new ArrayList<String>();
